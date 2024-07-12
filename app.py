0dace52546806dc5caae91a45b1b65053698c116
emad-eldin1 <emadeldin591970@gmail.com>
2024-07-13 02:01:48 +0300

    python file of app

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    operation = request.form['operation']
+
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'subtract':
+        result = num1 - num2
+    elif operation == 'multiply':
+        result = num1 * num2
+    elif operation == 'divide':
+        result = num1 / num2
+    else:
+        result = 'Invalid operation'
+
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
